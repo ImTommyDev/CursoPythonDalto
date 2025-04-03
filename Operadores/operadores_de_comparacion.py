d64157414560677a870d0b35eddf3f02ef0a4086
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-03 21:16:54 +0200

    Clase de comparadores terminada

---
+++ Operadores/operadores_de_comparacion.py
@@ -0,0 +1,23 @@
+#Operadores de comparación
+
+es_igual = 10 == 10 # Compara si son iguales
+es_diferente = 10 != 5 # Compara si son diferentes
+es_mayor = 10 > 5 # Compara si el primer número es mayor que el segundo
+es_menor = 10 < 5 # Compara si el primer número es menor que el segundo
+es_mayor_igual = 10 >= 5 # Compara si el primer número es mayor o igual que el segundo
+es_menor_igual = 10 <= 5 # Compara si el primer número es menor o igual que el segundo
+
+#Muestra los resultados
+print("Es igual:", es_igual)
+print("Es diferente:", es_diferente)
+print("Es mayor:", es_mayor)
+print("Es menor:", es_menor)
+print("Es mayor o igual:", es_mayor_igual)
+print("Es menor o igual:", es_menor_igual)
+
+#Comparación de cadenas
+cadena1 = "Hola"
+cadena2 = "Hola"
+cadena3 = "Adiós"
+son_iguales = cadena1 == cadena2 # Compara si son iguales
+son_diferentes = cadena1 != cadena3 # Compara si son diferentes
