afdb81f5c3e820d39fb633070fa684c3dbf4363c
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-03 16:47:16 +0200

    Clase de datos simples completada

--- Variables/variables.py
+++ Variables/variables.py
@@ -10,3 +10,11 @@ print(bienvenida + " " + nombre)
 
 edad = f"Tu edad es {c}?"
 print(edad)
+
+del edad # Eliminar la variable edad
+#print(edad) # Esto generará un error porque la variable edad ya no existe
+
+print("ola" in bienvenida) # Verifica si "ola" está en la variable bienvenida
+print("ola" not in bienvenida) # Verifica si "ola" no está en la variable bienvenida
+print("Ola" in bienvenida) # Verifica si "Ola" está en la variable bienvenida (case-sensitive)
+
