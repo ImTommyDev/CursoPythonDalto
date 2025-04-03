26514230556c9dc5dc029641bc793cefe3ed7b7a
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-03 16:39:21 +0200

    Primer commit del curso de Python de Dalto

---
+++ Tipos de datos/datosSimples.py
@@ -0,0 +1,15 @@
+#Strings
+"string"
+""""tus datos son:
+    Nombre:Tomas
+    Apellido: Primo"""
+''''String de 
+multiples lineas'''
+
+#Número
+40
+40.5
+
+#Booleano
+True
+False
---
+++ Variables/variables.py
@@ -0,0 +1,12 @@
+a = 2
+b = 3
+c = a + b
+c += 10
+print(c)
+
+bienvenida = "Hola"
+nombre = "Juan"
+print(bienvenida + " " + nombre)
+
+edad = f"Tu edad es {c}?"
+print(edad)
