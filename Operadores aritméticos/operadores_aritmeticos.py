9adacb862f441f0bbf5dda41f6c182b5cd1a0601
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-03 21:12:38 +0200

    Clase de los operadores aritméticos terminada

---
+++ Operadores aritméticos/operadores_aritmeticos.py
@@ -0,0 +1,15 @@
+suma = 10 + 5 # Suma
+resta = 10 - 5 #Resta
+multiplicacion = 10 * 5 # Multiplicación
+division = 10 / 5 # La división siempre devuelve un float
+modulo = 10 % 5 # Módulo (resto de la división)
+potencia = 10 ** 5 #Exponente
+division_entera = 10 // 5 # División entera. Devuelve entero redondeado hacia abajo (2.9 será 2)
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("Módulo:", modulo)
+print("Potencia:", potencia)
+print("División entera:", division_entera)
