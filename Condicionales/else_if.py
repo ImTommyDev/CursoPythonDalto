2b0f3f65b95cf99cab16df79ccd5c2aec8b02d8e
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-03 21:24:06 +0200

    Clase de if-else y elif terminada

---
+++ Condicionales/else_if.py
@@ -0,0 +1,8 @@
+ingreso_mensual = 1200
+
+if ingreso_mensual < 1000:
+    print("Ingreso mensual bajo")
+elif ingreso_mensual < 2000:
+    print("Ingreso mensual medio")
+else:
+    print("Ingreso mensual alto")
---
+++ Condicionales/if_else.py
@@ -0,0 +1,6 @@
+if 2 == 2:
+    print("Es verdadero")
+else:
+    print("Es falso")
+    
+print("Fin del programa")
