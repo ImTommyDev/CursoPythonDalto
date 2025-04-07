6948f8b11cc00e35269a5932d76bf4d6c3135529
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-07 23:26:31 +0200

    Usando parámetros opcionales y por defecto

--- Funciones/orden_parametros.py
+++ Funciones/orden_parametros.py
@@ -3,3 +3,10 @@ def saludo(nombre, edad):
     print(f"Hola {nombre}, tienes {edad} años")
     
 saludo(edad=25, nombre="Juan") # Cambiando el orden de los parámetros
+
+#Creando la misma función pero con un parámetro opcional
+def saludo(nombre, edad=18):
+    print(f"Hola {nombre}, tienes {edad} años")
+    
+saludo("Juan") # Si no se pasa el segundo parámetro, se toma el valor por defecto
+saludo("Juan", 25) # Si se pasan ambos parámetros, se usan los valores dados
