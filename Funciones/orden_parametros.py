77c8bde9b77a5418973a94da950b0a0b122c7088
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-07 23:25:14 +0200

    Cambiando el orden de los parámetros

---
+++ Funciones/orden_parametros.py
@@ -0,0 +1,5 @@
+#Cambiando el orden de los parámetros
+def saludo(nombre, edad):
+    print(f"Hola {nombre}, tienes {edad} años")
+    
+saludo(edad=25, nombre="Juan") # Cambiando el orden de los parámetros
