928a198114d0a42567e0a33a6b33905a5f330913
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-07 23:14:05 +0200

    Creando una función sumatoria que recibe parámetros y devuelve un valor

---
+++ Funciones/crear_funciones.py
@@ -0,0 +1,9 @@
+print("Creando una función que suma dos números")
+
+def suma(num1,num2):
+    return num1 + num2
+
+num1 = 2
+num2 = 3
+resultado = suma(num1,num2)
+print(f"La suma de {num1} y {num2} es: {resultado}")
