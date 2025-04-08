c2f2c368d31beb22068c5182535b77ed98037257
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-08 16:55:58 +0200

    Practicando con modulos

---
+++ Modulos/modulo_saludar.py
@@ -0,0 +1,11 @@
+def saludar(nombre):
+    """
+    Función que recibe un nombre y devuelve un saludo.
+    """
+    return f"Hola, {nombre}!"
+
+def calcular_suma(a, b):
+    """
+    Función que recibe dos números y devuelve su suma.
+    """
+    return a + b
---
+++ Modulos/modulos.py
@@ -0,0 +1,5 @@
+import modulo_saludar as m_saludar
+from modulo_saludar import calcular_suma as calcular
+
+print(m_saludar.saludar("Juan"))
+print(calcular(5, 10))
